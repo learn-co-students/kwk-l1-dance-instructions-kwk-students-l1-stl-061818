69c250b588226169542d5d8124ee11586285b5d7
aviflombaum <avi@flombaum.com>
2018-04-21 16:35:27 -0400

    Solution

--- macarena.rb
+++ macarena.rb
@@ -9,22 +9,22 @@ puts ""
 puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
-puts "Step 2: Turn your right palm up."
+puts "Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
+puts "Turn your right palm up."
 
 # Step 4: FIXME
-puts "Step 4: Turn your left palm up."
+puts "Turn your left palm up."
 
 # Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
+puts "Place your right hand on your left shoulder."
 
 # Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
+puts "Place your left hand on your right shoulder."
 
 # Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+puts "Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Bring your left hand up and around to the back your head."
